eacf4e06b5086768cf13f0fa40589fed34f47ffa
Stina Persson <stpe1901@student.miun.se>
2024-05-03 17:46:14 +0200

    gjort små ändringar

--- src/js/secrets.js
+++ src/js/secrets.js
@@ -5,3 +5,11 @@ let secretUser = localStorage.getItem('user');
 let secretDiv = document.getElementById("secretMessage"); 
 
 secretDiv.innerHTML = "Inloggad som " + secretUser; 
+
+async function getSecrets() {
+    let response = await fetch("http://127.0.0.1:3000/api/mypage"); 
+
+    let data = await response.json(); 
+
+    console.log(data); 
+}
